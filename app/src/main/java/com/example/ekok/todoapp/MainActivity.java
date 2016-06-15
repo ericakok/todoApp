f69c478d4d59bb5bb12e11952eb811ea1811b3a6
Erica Marie Seto Kok <ekok@fb.com>
2016-06-15 01:18:32 -0700

    can't get the final step working

--- app/src/main/java/com/example/ekok/todoapp/MainActivity.java
+++ app/src/main/java/com/example/ekok/todoapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.content.Intent;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
@@ -14,21 +16,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.io.File;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> todoItems;
     ArrayAdapter<String> aToDoAdapter;
     ListView lvItems;
     EditText etEditText;
+    private final int REQUEST_CODE = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         populateArrayItems();
-        lvItems = (ListView)findViewById(R.id.lvItems);
+        lvItems = (ListView) findViewById(R.id.lvItems);
         lvItems.setAdapter(aToDoAdapter);
         etEditText = (EditText) findViewById(R.id.etEditText);
+
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position,
@@ -39,7 +45,31 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                           long id) {
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                i.putExtra("mode", 2); // pass arbitrary data to launched activity
+                startActivityForResult(i, REQUEST_CODE);
+            }
+
+
+            protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+                // REQUEST_CODE is defined above
+                if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+                    // Extract name value from result extras
+                    String name = data.getExtras().getString("name");
+                    int code = data.getExtras().getInt("code", 0);
+                    // Toast the name to display temporarily on screen
+                    Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
     public void populateArrayItems() {
         readItems();
@@ -66,10 +96,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     public void onAddItem(View view) {
         aToDoAdapter.add(etEditText.getText().toString());
         etEditText.setText("");
         writeItems();
     }
+//
+//    // ActivityOne.java, time to handle the result of the sub-activity
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        // REQUEST_CODE is defined above
+//        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+//            // Extract name value from result extras
+//            String name = data.getExtras().getString("name");
+//            int code = data.getExtras().getInt("code", 0);
+//            // Toast the name to display temporarily on screen
+//            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+//        }
+//    }
+
 }
